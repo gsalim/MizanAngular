e0f7c674b287f32fd8415cc1720f7ec05d778fdd
gsalim <gsalim@192.168.1.107>
2017-09-18 20:49:07 +0600

    Startup

--- public_html/script/angular/app.js
+++ public_html/script/angular/app.js
@@ -1,14 +1,26 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp', []);
 
-app.value('AppInfo', {userId: '',version: '1.0.0',buildId: 'web.001'});
+app.value('AppInfo', {userId: '', version: '1.0.0', buildId: 'web.001'});
 
-app.controller("myCtrl",function($scope, AppInfo,$http){
+app.controller("myCtrl", function ($scope, AppInfo, $http) {
     $scope.name = "Alim";
     $scope.age = "30";
     $scope.version = AppInfo.version;
 });
 
-app.controller("myCtrl2",function($scope){
+app.controller("myCtrl2", function ($scope) {
     $scope.name = "Mizan";
     $scope.age = "24";
+});
+
+app.controller("formController", function ($scope) {
+
+    $scope.showInfo = function () {
+        $scope.output = "Your Name is " + $scope.name + " and id = " + $scope.id;
+    };
+
+    $scope.clearMessage = function () {
+        $scope.output = "";
+    };
+
 });
