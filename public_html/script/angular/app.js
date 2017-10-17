d2a3e9e4f5faf6f5c00d7d93a8b0723b88f7aa18
Dell <Dell@DESKTOP-CO377G5>
2017-10-17 18:27:01 +0600

    17-Oct-2017 | Evening

--- public_html/script/angular/app.js
+++ public_html/script/angular/app.js
@@ -1,26 +1,46 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ui.router']);
 
 app.value('AppInfo', {userId: '', version: '1.0.0', buildId: 'web.001'});
 
-app.controller("myCtrl", function ($scope, AppInfo, $http) {
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/home");
+
+        $stateProvider
+                .state('home', {
+                    url: "/home",
+                    templateUrl: "pages/home.html",
+                    controller: 'HomeController'
+                })
+                .state('login', {
+                    url: "/login",
+                    templateUrl: "pages/login.html"
+//                    controller: 'LoginCtrl'
+                });
+    }]);
+
+
+
+app.controller("HomeController", function ($scope, AppInfo, $http) {
     $scope.name = "Alim";
     $scope.age = "30";
     $scope.version = AppInfo.version;
 });
 
-app.controller("myCtrl2", function ($scope) {
-    $scope.name = "Mizan";
-    $scope.age = "24";
-});
-
-app.controller("formController", function ($scope) {
-
-    $scope.showInfo = function () {
-        $scope.output = "Your Name is " + $scope.name + " and id = " + $scope.id;
-    };
-
-    $scope.clearMessage = function () {
-        $scope.output = "";
-    };
-
-});
+//
+//app.controller("myCtrl2", function ($scope) {
+//    $scope.name = "Mizan";
+//    $scope.age = "24";
+//});
+//
+//app.controller("formController", function ($scope) {
+//
+//    $scope.showInfo = function () {
+//        $scope.output = "Your Name is " + $scope.name + " and id = " + $scope.id;
+//    };
+//
+//    $scope.clearMessage = function () {
+//        $scope.output = "";
+//    };
+//
+//});
